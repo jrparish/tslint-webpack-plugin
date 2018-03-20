acd42800676417cdaa67a43757002793f305b288
Jani <ljani@users.noreply.github.com>
2018-03-20 15:41:51 +0200

    Add typings

---
+++ index.d.ts
@@ -0,0 +1,26 @@
+declare module "tslint-webpack-plugin" {
+    import { Plugin } from "webpack";
+    import { Options as TslintOptions } from "tslint/lib/runner";
+
+    // https://github.com/DefinitelyTyped/DefinitelyTyped/blob/9f4c75126167d0d8af759f58405d53d983e94ad0/types/react-redux/index.d.ts#L33-L34
+    type Diff<T extends string, U extends string> = ({ [P in T]: P } & { [P in U]: never } & { [x: string]: never })[T];
+    type Omit<T, K extends keyof T> = Pick<T, Diff<keyof T, K>>;
+
+    type OmittedOptions = {
+        files: string[];
+        exclude: string[];
+    }
+
+    // tslint-webpack-plugin is more liberal with the options than tslint's runner is, thus tweak the interface
+    interface Options extends Omit<TslintOptions, keyof OmittedOptions> {
+        files: string | string[];
+        exclude?: string[];
+        format?: string;
+    }
+
+    class TslintWebpackPlugin extends Plugin {
+        constructor(options?: Options);
+    }
+
+    export = TslintWebpackPlugin;
+}
