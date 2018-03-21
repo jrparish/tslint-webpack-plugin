11f48b690eb9d36a6d917c4a52febccdff0c8a36
Jani <ljani@users.noreply.github.com>
2018-03-21 14:29:20 +0200

    Typings: Prefer a namespace over declare module  (#13)
    * Prefer a namespace over declare module
    
    Apparently import * as alias syntax does not work with export = function:
    https://github.com/Microsoft/TypeScript/issues/5073
    
    * TslintOptions already includes format

--- index.d.ts
+++ index.d.ts
@@ -1,7 +1,13 @@
-declare module "tslint-webpack-plugin" {
-    import { Plugin } from "webpack";
-    import { Options as TslintOptions } from "tslint/lib/runner";
+import { Plugin } from "webpack";
+import { Options as TslintOptions } from "tslint/lib/runner";
 
+export = TslintWebpackPlugin;
+
+declare class TslintWebpackPlugin extends Plugin {
+    constructor(options?: TslintWebpackPlugin.Options);
+}
+
+declare namespace TslintWebpackPlugin {
     // https://github.com/DefinitelyTyped/DefinitelyTyped/blob/9f4c75126167d0d8af759f58405d53d983e94ad0/types/react-redux/index.d.ts#L33-L34
     type Diff<T extends string, U extends string> = ({ [P in T]: P } & { [P in U]: never } & { [x: string]: never })[T];
     type Omit<T, K extends keyof T> = Pick<T, Diff<keyof T, K>>;
@@ -15,12 +21,5 @@ declare module "tslint-webpack-plugin" {
     interface Options extends Omit<TslintOptions, keyof OmittedOptions> {
         files: string | string[];
         exclude?: string[];
-        format?: string;
     }
-
-    class TslintWebpackPlugin extends Plugin {
-        constructor(options?: Options);
-    }
-
-    export = TslintWebpackPlugin;
 }
